f2a7ac052e512d900d57d8401f7752582ab122c1
mouxf <291864737@qq.com>
2016-09-16 23:51:40 +0800

    create project

---
+++ src/main/java/com/talon/project/util/Print.java
@@ -0,0 +1,12 @@
+package com.talon.project.util;
+
+public class Print {
+
+	public void print(Object obj){
+		System.out.print(obj);
+	}
+	
+	public void println(Object obj){
+		System.out.println(obj);
+	}
+}
